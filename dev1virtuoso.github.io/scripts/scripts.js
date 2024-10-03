e351a949540b224998f3d4bb8d30bae492280b4d
Carson Wu <carson.developer1125@gmail.com>
2024-10-03 20:34:05 +0800

    updated user name

--- dev1virtuoso.github.io/scripts/scripts.js
+++ dev1virtuoso.github.io/scripts/scripts.js
@@ -83,7 +83,7 @@ document.addEventListener(
         subtitle: "Technical blog about GitHub Sponsors",
         author: "Carson Wu",
         date: "10 August 2024",
-        link: "https://github.com/Carson-We/Documentation/blob/main/Blog/2024/August/10_08_2024/20240810_02.md",
+        link: "https://github.com/dev1virtuoso/Documentation/blob/main/Blog/2024/August/10_08_2024/20240810_02.md",
         tag: ["GitHub Sponsors", "Open-Source", "GitHub"],
       },
       {
@@ -92,7 +92,7 @@ document.addEventListener(
           "Technical blog about Utility-beta.github.io Scheduled Events",
         author: "Carson Wu",
         date: "10 July 2024",
-        link: "https://github.com/Carson-We/Documentation/blob/main/Blog/2024/July/10_07_2024/20240710_02.md",
+        link: "https://github.com/dev1virtuoso/Documentation/blob/main/Blog/2024/July/10_07_2024/20240710_02.md",
         tag: ["Release", "Open-Source", "GitHub"],
       },
       {
@@ -101,7 +101,7 @@ document.addEventListener(
           "Technical blog about carson1125 'v1.1.5.0(004)(1150_004-290624r)' update",
         author: "Carson Wu",
         date: "29 June 2024",
-        link: "https://github.com/Carson-We/Documentation/blob/main/Blog/2024/June/28_06_2024/20240628_02.md",
+        link: "https://github.com/dev1virtuoso/Documentation/blob/main/Blog/2024/June/28_06_2024/20240628_02.md",
         tag: ["Release", "Open-Source", "GitHub"],
       },
       {
@@ -109,7 +109,7 @@ document.addEventListener(
         subtitle: "Technical blog about A.R.I.E.L.",
         author: "Carson Wu",
         date: "7 April 2024",
-        link: "https://github.com/Carson-We/Documentation/blob/main/Blog/2024/April/07_04_2024/20240402_02.md",
+        link: "https://github.com/dev1virtuoso/Documentation/blob/main/Blog/2024/April/07_04_2024/20240402_02.md",
         tag: ["Release", "Open-Source", "GitHub"],
       },
       {
@@ -118,7 +118,7 @@ document.addEventListener(
           "Announces Setback in Logic-Based Inference System Development",
         author: "Carson Wu",
         date: "4 April 2024",
-        link: "https://github.com/Carson-We/Documentation/blob/main/Blog/2024/April/04_04_2024/20240404_02.md",
+        link: "https://github.com/dev1virtuoso/Documentation/blob/main/Blog/2024/April/04_04_2024/20240404_02.md",
         tag: ["Release", "Open-Source", "GitHub"],
       },
       {
@@ -127,7 +127,7 @@ document.addEventListener(
           "Technical blog about carson1125 'v0.1.0.2 (1002304024b)' update",
         author: "Carson Wu",
         date: "3 April 2024",
-        link: "https://github.com/Carson-We/Documentation/blob/main/Blog/2024/April/03_04_2024/20240403_02.md",
+        link: "https://github.com/dev1virtuoso/Documentation/blob/main/Blog/2024/April/03_04_2024/20240403_02.md",
         tag: ["Release", "Open-Source", "GitHub"],
       },
       {
@@ -135,7 +135,7 @@ document.addEventListener(
         subtitle: "Technical blog about NeuralMorse",
         author: "Carson Wu",
         date: "2 April 2024",
-        link: "https://github.com/Carson-We/Documentation/blob/main/Blog/2024/April/02_04_2024/20240402_02.md",
+        link: "https://github.com/dev1virtuoso/Documentation/blob/main/Blog/2024/April/02_04_2024/20240402_02.md",
         tag: ["Release", "Open-Source", "GitHub"],
       },
     ];
